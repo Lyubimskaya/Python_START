025198ccc36f6aec4fb24d6b18657b25b004cb49
Lyubimskaya <112004922+Lyubimskaya@users.noreply.github.com>
2022-11-28 01:49:23 +0200

    Update task_4.py

--- HW_1/task_4.py
+++ HW_1/task_4.py
@@ -10,15 +10,4 @@
 3
 x < 0, y < 0
 """
-n = int(input('Введите номер четверти: '))
 
-if n == 1:
-    print('x > 0, y > 0')
-elif n == 2:
-    print('x < 0, y > 0')
-elif n == 3:
-    print('x < 0, y < 0')
-elif n == 4:
-    print('x > 0, y < 0')
-else:
-    print('Неверный ввод')

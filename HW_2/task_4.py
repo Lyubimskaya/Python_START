d08e5a42d513a2ecca6e98e585285c9ee6463d91
Екатерина Любимская <knorotova@bk.ru>
2022-12-02 08:20:10 +0200

    i

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -11,7 +11,7 @@ num = int(input("Введите число: "))
 new_list = [random.randint(-num, num) for r in range(num)]
 index_list = []
 res = 1
-with open('indexes.txt', encoding = 'UTF-8') as data:
+with open('Users\lyubi\python2\Python_START\HW_2\indexes.txt', 'r' ) as data:
     for index in data:
         index_list.append(int(index))
 for i in range(-num, num):

0e330e91ff1b7c75ee4ae5c6aa0b84e22289c122
Екатерина Любимская <knorotova@bk.ru>
2022-12-20 21:42:50 +0200

    Задача 2

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,12 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+
+import random
+
+number = int(input('Введите размер списка: '))
+
+new_list = [random.randint(0, number) for i in range(number)]
+uni = [i for i in new_list if new_list.count(i) == 1]
+print(new_list)
+print(uni)
